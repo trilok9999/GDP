d3174ebe738d93cbd488ec9b37f6e979303edf20
S521950 <s521950@mail.nwmissouri.edu>
2015-07-07 21:29:17 -0500

    Material Design

--- app/src/main/java/com/example/s521950/gdp/MainActivity.java
+++ app/src/main/java/com/example/s521950/gdp/MainActivity.java
@@ -5,30 +5,35 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 
 public class MainActivity extends ActionBarActivity {
 ActionBar.Tab tab1,tab2,tab3,tab4,tab5;
+Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-        actionBar.setDisplayShowHomeEnabled(false);
-        actionBar.setDisplayShowTitleEnabled(false);
-
-        tab1 = actionBar.newTab().setText("Tab1").setTabListener(new TabListener<SportFragment>(this,"tennis",SportFragment.class));
-
-        tab2 = actionBar.newTab().setText("Tab2").setTabListener(new TabListener<ImageFragment>(this,"bask",ImageFragment.class));
-        tab3 = actionBar.newTab().setText("Tab3").setTabListener(new TabListener<AddSport>(this,"rugby",AddSport.class));
-
-
-        actionBar.addTab(tabA);
-        actionBar.addTab(tabB);
-        actionBar.addTab(tabC);
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        toolbar.setTitle("trilok");
+        toolbar.setLogo(R.drawable.ic_launcher);
+        setSupportActionBar(toolbar);
+//        ActionBar actionBar = getActionBar();
+//        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+//
+//        actionBar.setDisplayShowHomeEnabled(false);
+//        actionBar.setDisplayShowTitleEnabled(false);
+//
+//        tab1 = actionBar.newTab().setText("Tab1").setTabListener(new TabListener<SportFragment>(this,"tennis",SportFragment.class));
+//
+//        tab2 = actionBar.newTab().setText("Tab2").setTabListener(new TabListener<ImageFragment>(this,"bask",ImageFragment.class));
+//        tab3 = actionBar.newTab().setText("Tab3").setTabListener(new TabListener<AddSport>(this,"rugby",AddSport.class));
+//
+//
+//        actionBar.addTab(tabA);
+//        actionBar.addTab(tabB);
+//        actionBar.addTab(tabC);
     }
 
 
